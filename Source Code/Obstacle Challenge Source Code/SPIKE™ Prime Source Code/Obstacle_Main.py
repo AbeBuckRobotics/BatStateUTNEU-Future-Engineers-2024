da413822a45414993d3504f5ca6c392859556f5b
AbeBuck <141907756+AbeBuck@users.noreply.github.com>
2024-11-16 22:47:29 +0800

    Update Obstacle_Main.py

--- Source Code/Obstacle Challenge Source Code/SPIKE™ Prime Source Code/Obstacle_Main.py
+++ Source Code/Obstacle Challenge Source Code/SPIKE™ Prime Source Code/Obstacle_Main.py
@@ -1,47 +1,25 @@
-# from FE_Start import *
-from Obstacle_Start import obstacleStart;
-from Obstacle_LoopClockwise import obstacleLoopClockwise;
-from Obstacle_LoopCounter import obstacleLoopCounter;
-from Obstacle_Parking import obstacleParking;
-from pybricks.tools import wait, StopWatch;
+from Obstacle_Start import obstacleStart
+from Obstacle_Clockwise import obstacleClockwise
+from Obstacle_Counter import obstacleCounter
+from Obstacle_UTurn import obstacleUTurn
+from Obstacle_Parking import obstacleParking
+from pybricks.tools import wait, StopWatch
 
-# try:
-print("\n\n\n");
-_timer = StopWatch();
-multipleProgramsMenuNumber = 0;
+_clock = StopWatch()
+_robotLaps, _recordListMain = 0, [[None for y in range(4)] for x in range(4)]
 
-if (multipleProgramsMenuNumber == 0):
-    _robotLaps, _timeStraightList, _recordListMain = 0, [], [["", "", "", ""], ["", "", "", ""], ["", "", "", ""], ["", "", "", ""]];
+_clock.reset()
+_robotDirection = obstacleStart()
+_timeStart = _clock.time()
 
-    _timer.reset();
-    _robotDirection = obstacleStart();
-    # _robotDirection = 1;
-    _timeStart = _timer.time();
+while (_robotLaps < 12):
+    _robotLaps += 1
 
-    while (_robotLaps < 12):
-        _robotLaps += 1;
-        
-        print("\nLaps:  " + str(_robotLaps / 4));
+    print(f"Laps: {_robotLaps / 4}")
 
-        _timer.reset();
-        _recordListMain[_robotLaps % 4] = obstacleLoopClockwise(_recordListMain[_robotLaps % 4]) if (_robotDirection == 1) else obstacleLoopCounter(_recordListMain[_robotLaps % 4]);
-        print(_recordListMain);
-        _timeStraightList.append(_timer.time());
+    _recordListMain[_robotLaps % 4] = obstacleClockwise(_recordListMain[_robotLaps % 4], _robotLaps) if (_robotDirection == 1) else obstacleCounter(_recordListMain[_robotLaps % 4], _robotLaps)
+    # _recordListMain = [[None for y in range(4)] for x in range(4)]
 
-    _timer.reset();
-    obstacleParking(_robotDirection, _recordListMain);
-    _timeParking = _timer.time();
+obstacleParking(_robotDirection, _recordListMain)
 
-# except Exception as error:
-#   print("An error occurred:", type(error).__name__, "–", error);
-
-# finally:
-#     pass;
-#     print("\n" + str(_timeStraightList));
-#     _timeStraightListSum = 0;
-
-#     for i in range (len(_timeStraightList)):
-#         _timeStraightListSum += _timeStraightList[i];
-
-#     print("Start:", _timeStart, "\tLoop:", _timeStraightListSum, "\tTotal:", (_timeStart + _timeStraightListSum), end = "\t");
-#     print("Parking:", _timeParking);
+print(f"\nFINAL TIME: {_clock.time()}")
