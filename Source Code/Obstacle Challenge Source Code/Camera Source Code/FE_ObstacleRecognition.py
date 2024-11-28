f333b06863f58c8bb32eece5d7c232ab1e07063b
AbeBuck <141907756+AbeBuck@users.noreply.github.com>
2024-11-29 06:07:10 +0800

    Internationals Practice Run Threshold Updates

--- Source Code/Obstacle Challenge Source Code/Camera Source Code/FE_ObstacleRecognition.py
+++ Source Code/Obstacle Challenge Source Code/Camera Source Code/FE_ObstacleRecognition.py
@@ -1,6 +1,12 @@
 import sensor, time
+import pyb
 from micropython import const
 from pupremote import PUPRemoteSensor
+
+contestMode = const(True)
+
+rLED = pyb.LED(1)
+gLED = pyb.LED(2)
 
 camera = PUPRemoteSensor(power = True)
 camera.add_channel('blob', to_hub_fmt = 'hhhhhh')
@@ -8,10 +14,8 @@ camera.add_channel('blob', to_hub_fmt = 'hhhhhh')
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames(time = 1000)
 sensor.set_vflip(True)
 sensor.set_hmirror(True)
-clock = time.clock()
 
 screenWidth = sensor.width()
 screenHeight = sensor.height()
@@ -23,45 +27,108 @@ roi = (roiX, roiY, roiWidth, roiHeight)
 
 sensor.set_windowing(roi)
 sensor.set_framerate(15)
-# to consume less power
 
-contestMode = False
+sensor.skip_frames(time = 1000)
 
-_GREEN = const((0, 100, -128, -10, 20, 127))
-_RED = const((0, 100, 7, 127, -10, 127))
+sensor.set_auto_gain(False)  # must be turned off for color tracking
+sensor.set_auto_whitebal(False)  # must be turned off for color tracking
+
+clock = time.clock()
+
+_GREEN = const((10, 90, -128, -15, 0, 127))
+_RED = const((10, 90, 7, 127, 3
+, 127))
 # format: (Lmin, Lmax, Amin, Amax, Bmin, Bmax)
 
-while True:
-    img = sensor.snapshot()
+# _GREEN = const((0, 100, -128, -10, 20, 127))
+# _RED = const((0, 100, 7, 127, -10, 127))
 
-    gBlobs = img.find_blobs([_GREEN], pixels_threshold = 400)
-    gBlob, gPix, gCx, gCy = None, 0, 0, 0
+if (contestMode):
+    while True:
+        img = sensor.snapshot()
 
-    for g in gBlobs:
-        if ((g.h() > g.w() or (g.y() + g.h()) == roiHeight) and g.pixels() > gPix):
-            gPix = g.pixels()
-            gBlob = g
+        gBlobs = img.find_blobs([_GREEN], pixels_threshold = 350)
+        gBlob, gPix, gCx, gCy = None, 0, 0, 0
 
-    if (gBlob != None):
-        gDen = gBlob.density()
-        gCx = gBlob.cx()
-        gCy = gBlob.cy()
+        for g in gBlobs:
+            if ((g.h() > g.w() or (g.y() + g.h()) == roiHeight) and g.pixels() > gPix):
+                gPix = g.pixels()
+                gBlob = g
 
-        img.draw_rectangle(gBlob.rect(), (238, 39, 55), 1)
+        if (gBlob != None):
+            gDen = gBlob.density()
+            gCx = gBlob.cx()
+            gCy = gBlob.cy()
 
-    rBlobs = img.find_blobs([_RED], pixels_threshold = 400)
-    rBlob, rPix, rCx, rCy = None, 0, 0, 0
+        rBlobs = img.find_blobs([_RED], pixels_threshold = 350)
+        rBlob, rPix, rCx, rCy = None, 0, 0, 0
 
-    for r in rBlobs:
-        if (r.pixels() > rPix):
-            rPix = r.pixels()
-            rBlob = r
+        for r in rBlobs:
+            if (r.pixels() > rPix):
+                rPix = r.pixels()
+                rBlob = r
 
-    if (rBlob != None):
-        rCx = rBlob.cx()
-        rCy = rBlob.cy()
-        img.draw_rectangle(rBlob.rect(), color = (68, 214, 44), thickness = 1)
+        if (rBlob != None):
+            rCx = rBlob.cx()
+            rCy = rBlob.cy()
 
-    print(gPix, rPix)
-    camera.update_channel('blob', gCx, gCy, gPix, rCx, rCy, rPix)
-    camera.process()
+        if (gPix > rPix):
+            gLED.on()
+            rLED.off()
+        elif (rPix > gPix):
+            gLED.off()
+            rLED.on()
+        else:
+            gLED.off()
+            rLED.off()
+
+        camera.update_channel('blob', gCx, gCy, gPix, rCx, rCy, rPix)
+        camera.process()
+
+
+
+else:
+    while True:
+        img = sensor.snapshot()
+
+        gBlobs = img.find_blobs([_GREEN], pixels_threshold = 350)
+        gBlob, gPix, gCx, gCy = None, 0, 0, 0
+
+        for g in gBlobs:
+            if ((g.h() > g.w() or (g.y() + g.h()) == roiHeight) and g.pixels() > gPix):
+                gPix = g.pixels()
+                gBlob = g
+
+        if (gBlob != None):
+            gDen = gBlob.density()
+            gCx = gBlob.cx()
+            gCy = gBlob.cy()
+
+            img.draw_rectangle(gBlob.rect(), (238, 39, 55), 1)
+
+        rBlobs = img.find_blobs([_RED], pixels_threshold = 350)
+        rBlob, rPix, rCx, rCy = None, 0, 0, 0
+
+        for r in rBlobs:
+            if (r.pixels() > rPix):
+                rPix = r.pixels()
+                rBlob = r
+
+        if (rBlob != None):
+            rCx = rBlob.cx()
+            rCy = rBlob.cy()
+            img.draw_rectangle(rBlob.rect(), color = (68, 214, 44), thickness = 1)
+
+        if (gPix > rPix):
+            gLED.on()
+            rLED.off()
+        elif (rPix > gPix):
+            gLED.off()
+            rLED.on()
+        else:
+            gLED.off()
+            rLED.off()
+
+        print(gPix, rPix)
+        camera.update_channel('blob', gCx, gCy, gPix, rCx, rCy, rPix)
+        camera.process()
